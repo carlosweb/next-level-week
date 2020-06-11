be329da319ee2cee0822246fb3cf33da1967e606
carlosweb <kaduinfo@gmail.com>
2020-06-11 01:31:47 -0300

    Commit base

---
+++ app.js
@@ -0,0 +1,18 @@
+// "use strict"
+
+// const gulp = require("gulp")
+// const sass = require("gulp-sass")
+
+// sass.compiler = require("node-sass")
+// gulp.task('sass', compilaSass)
+
+// function compilaSass() {
+//     return gulp
+//     .src("css/**/*.scss")
+//     .pipe(sass())
+//     .pipe(gulp.dest(css))
+// }
+
+// function watch() {
+//     gulp.watch("css/**/*.scss", compilaSass)
+// }
