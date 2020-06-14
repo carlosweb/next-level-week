e8f8392d3d552b1b423233d1d11dbaf03e798215
carlosweb <kaduinfo@gmail.com>
2020-06-14 13:41:28 -0300

    Arrumando as pastas

---
+++ src/server.js
@@ -0,0 +1,30 @@
+// server
+const express = require("express")
+const server = express()
+
+// how to show files
+server.use(express.static("public"))
+
+// how to use template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// config routes
+server.get("/", (req, res) => {
+    return res.render("index.html", {title: "Seu Marketplace de coleta de Resíduos"})
+})
+
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+ })
+
+
+// start server
+server.listen(3000) 
